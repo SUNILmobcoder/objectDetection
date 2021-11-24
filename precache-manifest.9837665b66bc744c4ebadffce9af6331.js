3413c8700247bc04a42b30bcb9032ec038e4b5ef
SUNILmobcoder <sunil.mobcoder@gmail.com>
2021-11-24 17:23:34 +0530

    Updates

---
+++ precache-manifest.9837665b66bc744c4ebadffce9af6331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a592e3feaff502bcf8dde339e6872d4",
+    "url": "/objectDetection/index.html"
+  },
+  {
+    "revision": "de6058fdc69a0e3fea32",
+    "url": "/objectDetection/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "a05491ca2ad211d8d5bf",
+    "url": "/objectDetection/static/js/2.d4386b2d.chunk.js"
+  },
+  {
+    "revision": "127c34995248f837456ddaca21e714fd",
+    "url": "/objectDetection/static/js/2.d4386b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de6058fdc69a0e3fea32",
+    "url": "/objectDetection/static/js/main.f448a300.chunk.js"
+  },
+  {
+    "revision": "72205a1ab587cb9d773d",
+    "url": "/objectDetection/static/js/runtime-main.377bfcf2.js"
+  }
+]);
